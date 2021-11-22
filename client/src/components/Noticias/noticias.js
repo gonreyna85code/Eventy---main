ebcf5c90008e6833b9a9ee30db71ef5450ed663e
acuervov <acuervov@unal.edu.co>
2021-11-22 12:10:27 -0500

    Se añadio componente de noticias, falta css e incorporar componente cards

---
+++ client/src/components/Noticias/noticias.js
@@ -0,0 +1,17 @@
+import react from 'react'
+import './noticias.css'
+
+
+export default function noticias (){
+
+    return (
+        <div id="containerNoticias">
+            <h1 id="title">ÚLTIMAS NOTICIAS</h1>
+            <div id="cartas">
+                <div id="example"></div>
+                <div id="example"></div>
+                <div id="example"></div>
+            </div>
+        </div>
+    )
+}
