6ddf726ea58588e9ca00679f10947dea5b17e16c
acuervov <acuervov@unal.edu.co>
2021-11-24 12:56:48 -0500

    Se hañadio la funcionalidad al componente searchBar y se agrego sus respectivas actions y reducer

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Boton from "../Boton/Boton"
+import { useDispatch } from "react-redux";
+import { findEvent } from "../../redux/actions";
 
 
-export function searchBar (){
+export default function SearchBar (){
     const [state, setState] = React.useState(""); 
+    const dispatch = useDispatch();
 
-    function search(){}
+    function search(event){
+        event.preventDefault();
+        dispatch(findEvent(state))
+    }
+
     return (
         <div>
-            <input value = {state} placeholder="Busqueda" onChange={setState(value)}/>
+            <input value = {state} placeholder="Busqueda" onChange={event => setState(event.target.value)}/>
             <Boton colorBtn="btn_naranja" onClick={search}>Buscar</Boton>
         </div>
     )
 }
+
