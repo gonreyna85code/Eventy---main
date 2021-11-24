135bcfc1bed76cfda20cfbf28ad2eb25e14acad0
gnunez-dev <gnunez.dev@gmail.com>
2021-11-23 21:50:24 -0500

    Agregue url /crear-evento

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/home.js'
 import Userform from './components/Userform';
 import HomeUser from './components/HomeUser/HomeUser';
+import CrearEventos from './components/CrearEventos/CrearEventos';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path = '/' element ={<Home/>}/>
         <Route exact path = '/login' element = {<Userform/>}/>
         <Route exact path = '/homeuser' element = {<HomeUser/>}/>
+        <Route exact path = '/crear-evento' element = {<CrearEventos/>}/>
       </Routes>
     </div>
     </BrowserRouter>
