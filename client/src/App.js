443d62d8a250f6a04e850d7334b80d3347211683
lautaro <kautarol@gmail.com>
2021-11-23 22:26:11 -0300

    Cards de Eventos hardcodeado en el home

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './components/home.js'
-import Userform from './components/Userform';
-import HomeUser from './components/home/home';
+import Landing from './Routes/Landing'
+import Userform from './Routes/Userform';
+import Home from './Routes/home';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
-        <Route exact path = '/' element ={<Home/>}/>
+        <Route exact path = '/' element ={<Landing/>}/>
         <Route exact path = '/login' element = {<Userform/>}/>
-        <Route exact path = '/homeuser' element = {<HomeUser/>}/>
+        <Route exact path = '/homeuser' element = {<Home/>}/>
       </Routes>
     </div>
     </BrowserRouter>
