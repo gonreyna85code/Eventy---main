1c6a15d82af3a5d74fe01bde8c0d2e9dcc010f6e
Alfredo-Calvelo <alfredocalvelo1@gmail.com>
2021-11-25 16:09:21 -0300

    Back de user modificado

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Landing from './Routes/Landing'
-import Userform from './Routes/Userform';
-import Home from './Routes/home';
+import LogingForm from './Routes/LogingForm';
+import Landing from './Routes/Landing';
 import CrearEventos from './Routes/CrearEventos/CrearEventos';
 import NavBar from "./Routes/NavBar/NavBar"
 
@@ -12,8 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path = '/' element ={<Landing/>}/>
-        <Route exact path = '/login' element = {<Userform/>}/>
-        <Route exact path = '/homeuser' element = {<Home/>}/>
+        <Route exact path = '/login' element = {<LogingForm/>}/>
         <Route exact path = '/crear-evento' element={<div>
           <NavBar/>
           <CrearEventos/>
