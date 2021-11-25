e5860db08963daf6e1f4a33013a4c72a2828eac1
acuervov <acuervov@unal.edu.co>
2021-11-24 19:12:31 -0500

    Se termino el navBar incluyendo el menu desplegable, falta mejorar aspecto

--- client/src/Routes/NavBar/NavBar.js
+++ client/src/Routes/NavBar/NavBar.js
@@ -3,15 +3,24 @@ import SearchBar from '../../components/SearchBar/SearchBar'
 import { Link } from "react-router-dom";
 import logo from '../../Routes/images/logo-blanco.png'
 import styles from './NavBar.module.css'
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
  
 
 export default function NavBar(){
- 
+  
     return (
         <div className={styles.navBar}>
             <div className={styles.icono}><Link to ="/homeuser"><img src={logo}/></Link></div>
             <div className={styles.search}><SearchBar/></div>
-            <div className={styles.menu}>figurita</div>
+            <div className={styles.menu}>
+                <FontAwesomeIcon icon={faUser}/>
+                <ul>
+                    <li><Link to="/crear-evento" style={{textDecoration: 'none', color: 'black'}}><a>Crear Evento</a></Link></li>
+                    <li><Link to="/home" style={{textDecoration: 'none', color: 'black'}}><a>ruta 2</a></Link></li>
+                    <li><Link to="/home" style={{textDecoration: 'none', color: 'black'}}><a>ruta 3</a></Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
