39a030759948006485e4d51c1af65a93c0a7341c
lautaro <kautarol@gmail.com>
2021-11-25 16:52:29 -0300

    Componente de detalle de evento terminado

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,25 @@ import Landing from './Routes/Landing'
 import Userform from './Routes/Userform';
 import Home from './Routes/home';
 import CrearEventos from './Routes/CrearEventos/CrearEventos';
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import DetailEvet from './Routes/DetailEvent';
-=======
-import NavBar from "./Routes/NavBar/NavBar"
->>>>>>> 7927f2b4e2f4f8cbb4385cd25d20948936448065
+//=======
+//import NavBar from "./Routes/NavBar/NavBar"
+//>>>>>>> 7927f2b4e2f4f8cbb4385cd25d20948936448065
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
-        <Route exact path = '/' element ={<Landing/>}/>
+        <Route exact path = '/' element ={<Landing/>}/> 
         <Route exact path = '/login' element = {<Userform/>}/>
         <Route exact path = '/homeuser' element = {<Home/>}/>
-<<<<<<< HEAD
         <Route exact path = '/crear-evento' element = {<CrearEventos/>}/>
         <Route exact path = '/detailEvent/:name' element = {<DetailEvet/>}/>
-=======
         <Route exact path = '/crear-evento' element={<div>
-          <NavBar/>
           <CrearEventos/>
         </div>}/>
->>>>>>> 7927f2b4e2f4f8cbb4385cd25d20948936448065
       </Routes>
     </div>
     </BrowserRouter>
--- client/src/Routes/DetailEvent.js
+++ client/src/Routes/DetailEvent.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getEvent } from "../redux/actions";
+import './DetailEvents.css';
+import Boton from "../components/Boton/Boton";
 
 export default function DetailEvet(){
     
@@ -23,13 +25,33 @@ export default function DetailEvet(){
            {
                theEvent?
                <div>
-                   <h1>{theEvent.name}</h1>
+                   <h1 className = 'nombre_evento'>{theEvent.name}</h1>
+                   <div>
+                   <img className = 'imagen_detail' src = 'https://www.masquenegocio.com/wp-content/uploads/2018/03/evento-concierto-874x492.jpg'></img>
+                   <p>Esta imágen solo es de prueba</p>
+                   </div>
+                   <div>
+                       <h1>Información del evento:</h1>
+                       <h3>Lugar:</h3>
+                       <p>{theEvent.location}</p>
+                       <h3>Fecha y hora:</h3>
+                       <p>{theEvent.date}</p>
+                       <p>Esta es una fecha de prueba</p>
+                   </div>
+                   <div>
+                       <h1>Comprar entradas:</h1>
+                       <p>Aquí iría el sistema de pagos.</p>
+                   </div>
+                   <div>
+                       <h1>Mapa:</h1>
+                       <p>Aquí iría el mapa del lugar desde una api externa</p>
+                   </div>
                </div>
                :
                <h1>Cargando... </h1>
            }
            <Link to ='/homeuser'>
-               <button> Volver al Home </button>
+           <Boton colorBtn='btn_azul'>Volver al Home</Boton>
            </Link>
        </div>
     )
