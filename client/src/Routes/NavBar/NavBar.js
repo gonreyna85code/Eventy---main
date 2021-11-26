b4a11a4127f6be3ec72290d00b206be7ae71ffef
gonzalo <gonreyna85@gmail.com>
2021-11-26 19:44:14 -0300

    logout implementado

--- client/src/Routes/NavBar/NavBar.js
+++ client/src/Routes/NavBar/NavBar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import SearchBar from '../../components/SearchBar/SearchBar'
 import { Link } from "react-router-dom";
+import { useDispatch} from "react-redux";
+import { useNavigate } from 'react-router';
 import logo from '../../Routes/images/logo-blanco.png'
 import styles from './NavBar.module.css'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { logout } from "../../redux/actions";
  
 
-export default function NavBar(){
-  
+export default function NavBar(){  
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const handler = () => {
+        dispatch(logout());
+        window.location.reload(true);
+    }
     return (
         <div className={styles.navBar}>
             <div className={styles.icono}><Link to ="/homeuser"><img src={logo} alt=''/></Link></div>
@@ -18,7 +26,7 @@ export default function NavBar(){
                 <ul>
                     <li><Link to="/crear-evento" style={{textDecoration: 'none', color: 'black'}}><span>Crear Evento</span></Link></li>
                     <li><Link to="/home" style={{textDecoration: 'none', color: 'black'}}><span>ruta 2</span></Link></li>
-                    <li><Link to="/home" style={{textDecoration: 'none', color: 'black'}}><span>ruta 3</span></Link></li>
+                    <li><Link to="/" onClick={(e) => handler()} style={{textDecoration: 'none', color: 'black'}}><span>Logout</span></Link></li>
                 </ul>
             </div>
         </div>
