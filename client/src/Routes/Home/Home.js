fb518723616620a31b2ff0c3295cdd659938af41
gonzalo <gonreyna85@gmail.com>
2021-11-26 13:13:22 -0300

    home muestra eventos cercanos

--- client/src/Routes/Home/Home.js
+++ client/src/Routes/Home/Home.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import styles from './Home.module.css';
 import NavBar from '../NavBar/NavBar';
 import EventHome from './EventHome';
-import { getUser, getNearbyEvents } from '../../redux/actions';
+import { getUser } from '../../redux/actions';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import './HomeCarrusel.css';
@@ -25,15 +25,13 @@ const Home = () => {
     
     const dispatch = useDispatch();
     const user = useSelector( state => state.User );
-    const eventosCercanos = useSelector(state => state.NearbyEvents)
+    const eventosCercanos = user.near
 
     useEffect(()=>{
         dispatch(getUser());
     }, [dispatch]);
 
-    useEffect(()=>{
-        dispatch(getNearbyEvents(user.profile.city));
-    }, [dispatch, user.profile.city]);
+    
     
     console.log({eventosCercanos})
 
@@ -50,7 +48,7 @@ const Home = () => {
                     <AliceCarousel
                         mouseTracking
                         items={
-                            user.events && user.events.map( evento => {
+                            user.near && user.near.map( evento => {
                                 return(
                                     <EventHome
                                     name={evento.name}
