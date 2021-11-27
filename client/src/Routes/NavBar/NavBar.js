2559626fc717281a48a035c71cc5f2ee5be556b1
gonzalo <gonreyna85@gmail.com>
2021-11-27 17:30:32 -0300

    user en navbar

--- client/src/Routes/NavBar/NavBar.js
+++ client/src/Routes/NavBar/NavBar.js
@@ -2,15 +2,17 @@ import React from "react";
 import SearchBar from '../../components/SearchBar/SearchBar'
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import logo from '../../Routes/images/logo-blanco.png'
 import styles from './NavBar.module.css'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faUser} from '@fortawesome/free-solid-svg-icons';
 import { logout } from "../../redux/actions";
+
  
 
 export default function NavBar(){  
+    const user = useSelector((state) => state.User);
     let navigate = useNavigate();
     const dispatch = useDispatch();
     const handler = () => {
@@ -23,7 +25,8 @@ export default function NavBar(){
             <div className={styles.icono}><Link to ="/"><img src={logo} alt=''/></Link></div>
             <div className={styles.search}><SearchBar/></div>
             <div className={styles.menu}>
-                <FontAwesomeIcon icon={faUser}/>
+             <FontAwesomeIcon icon={faUser}/>
+             {user.username}
                 <ul>
                     <li><Link to="/crear-evento" style={{textDecoration: 'none', color: 'black'}}><span>Crear Evento</span></Link></li>
                     <li><Link to="/profile" style={{textDecoration: 'none', color: 'black'}}><span>Profile</span></Link></li>
