c2cdd23254128dc0b9c9b889b4018fb0fcf6dda5
Alfredo-Calvelo <alfredocalvelo1@gmail.com>
2021-11-28 16:21:17 -0300

    Rutas corregidas

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Landing from './components/Landing/Landing';
 import CrearEventos from './Routes/CrearEventos/CrearEventos';
 import DetailEvet from './Routes/DetailEvent';
 import Profile from './Routes/Profile/Profile';
-
+import { Navigate } from 'react-router';
 import {getUser} from './redux/actions'
 import Home from './Routes/Home/Home';
 import SocialCategory from './Routes/SocialCategory/SocialCategory';
@@ -18,7 +18,6 @@ import SubCategory from './Routes/SocialCategory/SocialSubcategories/Socialsub';
 import NavBar from './components/NavBar/NavBar';
 import Loginform from './components/Login/LogingForm';
 import RegisterForm from './components/Login/RegisterForm';
-
 import AllEvents from './Routes/AllEvents/AllEvents';
 
 
@@ -37,16 +36,16 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path = '/' element ={ user && !user._id ? <Landing/>: <Home/> }/> 
-          <Route exact path = '/crear-evento' element = {user && !user._id ?<div><NavBar/><CrearEventos/></div>:<Landing/>}/>
-          <Route exact path = '/detailEvent/:name' element = {user && !user._id ?<div><NavBar/><DetailEvet/></div>:<Landing/>}/>
-          <Route exact path = '/login' element = {user && !user._id ?<Loginform/>:<Landing/>}/>
-          <Route exact path = '/createUser' element = {user && !user._id ?<RegisterForm/>:<Landing/>}/>
-          <Route exact path = '/profile' element = {user && !user._id ?<div><NavBar/><Profile/></div>:<Landing/>}/>
-          <Route exact path = '/setting' element = {user && !user._id ?<div><NavBar/><Setting/></div>:<Landing/>}/>
-          <Route exact path = '/social' element = {user && !user._id ?<div><NavBar/><SocialCategory/></div>:<Landing/>}/>
-          <Route exact path = '/sport' element = {user && !user._id ?<div><NavBar/><SportCategory/></div>:<Landing/>}/>
-          <Route exact path = '/subcategory/:subcategory' element = {user && !user._id ?<div><NavBar/><SubCategory/></div>:<Landing/>}/>
-          <Route exact path = '/result' element = {user && !user._id ?<div><NavBar/><Resultado/></div>:<Landing/>}/>
+          <Route exact path = '/crear-evento' element = {user && !user._id ?<div><NavBar/><CrearEventos/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/detailEvent/:name' element = {user && !user._id ?<div><NavBar/><DetailEvet/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/login' element = {user && user._id ?<Navigate to= '/'/>:<Loginform/>}/>
+          <Route exact path = '/createUser' element = {user && !user._id ?<RegisterForm/>:<Navigate to= '/'/>}/>
+          <Route exact path = '/profile' element = {user && !user._id ?<div><NavBar/><Profile/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/setting' element = {user && !user._id ?<div><NavBar/><Setting/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/social' element = {user && !user._id ?<div><NavBar/><SocialCategory/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/sport' element = {user && !user._id ?<div><NavBar/><SportCategory/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/subcategory/:subcategory' element = {user && !user._id ?<div><NavBar/><SubCategory/></div>:<Navigate to= '/'/>}/>
+          <Route exact path = '/result' element = {user && !user._id ?<div><NavBar/><Resultado/></div>:<Navigate to= '/'/>}/>
         </Routes>
       </div>
     </BrowserRouter>
