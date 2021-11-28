24741f6813903d5e4a388f0d3e7c5a03f4d3441a
gonzalo <gonreyna85@gmail.com>
2021-11-28 18:02:10 -0300

    all events logic

--- client/src/Routes/AllEvents/AllEvents.js
+++ client/src/Routes/AllEvents/AllEvents.js
@@ -1,7 +1,20 @@
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllEvents } from '../../redux/actions';
 import styles from './AllEvents.module.css';
 import FilterEvents from '../../components/FilterEvents/FilterEvents';
 
 const AllEvents = () => {
+    const dispatch = useDispatch();
+    const events = useSelector(state => state.AllEvents);
+    const cities = () => {
+      let array = events.map(event => event.city);
+        let unique = [...new Set(array)];   
+        return unique;
+    }
+    useEffect(()=>{
+        dispatch(getAllEvents());
+    }, [dispatch]);
 
     return(
         <div className={styles.cont_all_events}>
