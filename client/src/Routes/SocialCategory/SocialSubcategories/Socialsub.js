ff33df9da44931fa467f87dd7372a2dabccca135
lautaro <kautarol@gmail.com>
2021-11-29 17:19:33 -0300

    commit

--- client/src/Routes/SocialCategory/SocialSubcategories/Socialsub.js
+++ client/src/Routes/SocialCategory/SocialSubcategories/Socialsub.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { findEventSub } from "../../../redux/actions";
 import Card from "../../../components/CardEvent";
 import botonStyles from '../../../components/Boton/Boton.module.css';
+import Boton from '../../../components/Boton/Boton'
 
 export default function SubCategory(){
 
