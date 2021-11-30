92585ae6ab12c24f1c3191804129e71106e66a63
gonzalo <gonreyna85@gmail.com>
2021-11-30 13:56:37 -0300

    .

--- client/src/Routes/NavBar/NavBar.js
+++ client/src/Routes/NavBar/NavBar.js
@@ -1,13 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 import SearchBar from '../../components/SearchBar/SearchBar'
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router';
 import { useDispatch, useSelector } from "react-redux";
 import logo from '../../Routes/images/logo-blanco.png'
-import styles from './NavBar.module.css'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faUser, faEllipsisV} from '@fortawesome/free-solid-svg-icons';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
 import { logout } from "../../redux/actions";
+import botonStyles from "../../components/Boton/Boton.module.css"
+import styles from './NavBar.module.css'
+
 
  
 
@@ -15,42 +17,37 @@ export default function NavBar(){
     const user = useSelector((state) => state.User);
     let navigate = useNavigate();
     const dispatch = useDispatch();
-
-    const [menu, setMenu] = useState(false);
-
-    const handleClick = ()=>{
-        let value = menu ? false : true
-        setMenu(value)
-    }
-
     const handler = () => {
         dispatch(logout());
         navigate('/');
         window.location.reload(true);
     }
-
-
     return (
         <div className={styles.navBar}>
             <div className={styles.icono}><Link to ="/"><img src={logo} alt=''/></Link></div>
-            <div className={styles.cont_left}>
-                <div className={styles.search}><SearchBar/></div>
-                <div  className={menu ? `${styles.menu_active} ${styles.cont_menu}` : styles.cont_menu}>
-                    <FontAwesomeIcon icon={faEllipsisV} onClick={handleClick} className={styles.icon_menu}/>
+            <div className = {styles.menu}>
+            <div className={styles.search}><SearchBar/></div>
+                <div className={styles.desplegable}>
+                <Link to="/profile" style={{textDecoration: 'none'}}>
+                    <div className= {styles.button}>
+                        <div className ={styles.profileName}>
+                            {user.profile? user.profile.name:null}
+                        </div>
+                        <FontAwesomeIcon icon={faUser}/>
+                    </div>
+
+                </Link>
+
                     <ul>
-                        <li><Link to="/crear-evento">Crear Evento</Link></li>
-                        <li><Link to="/profile">Profile</Link></li>
-                        <li><Link to="/all-events">Eventos</Link></li>
-                        <li><Link to="/" onClick={(e) => handler()}>Logout</Link></li>
+                        <div ><Link to="/crear-evento" style={{textDecoration: 'none'}}><span>Crear Evento</span></Link></div>
+                        <div ><Link to="/" onClick={(e) => handler()} style={{textDecoration: 'none'}}><span>Logout</span></Link></div>
+                        {/* <div ><Link to="/setting" style={{textDecoration: 'none'}}><span>About</span></Link></div> */}
+
                     </ul>
                 </div>
-                <div className={styles.cont_user}>
-                    <Link to="/profile">
-                        {user.username}
-                        <FontAwesomeIcon icon={faUser}/>
-                    </Link>
-                </div>
+
             </div>
+
         </div>
     )
 }
