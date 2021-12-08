a24b287004be8fc2d30a6f722bf4f7b6ab788fa4
gonzalo <gonreyna85@gmail.com>
2021-12-08 19:28:17 -0300

    time out de espera provisorio en login

--- client/src/Routes/Login/LogingForm.js
+++ client/src/Routes/Login/LogingForm.js
@@ -73,7 +73,8 @@ export default function Loginform() {
             alert("Usuario Confirmado, Bienvenido a Eventy");
             dispatch(getUser());
             navigate('/');
-            window.location.reload();
+            setTimeout(function(){ window.location.reload(); }, 3000);
+            
             
           }}> INGRESAR </Boton>
         </div>
