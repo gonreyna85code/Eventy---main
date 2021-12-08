58321ab21033a771fd80c6128152b1535866104b
gonzalo <gonreyna85@gmail.com>
2021-12-08 18:36:20 -0300

    prueba 3

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -34,7 +34,7 @@ axios.defaults.withCrendentails = true;
 axios.defaults.Credentials = "includes";
 
 const local = "http://localhost:4000/"
-const heroku = "https://gonzalo-eventy3.herokuapp.com"
+const heroku = "https://gonzalo-eventy3.herokuapp.com/"
 
 
 export function changeUserCity(cityDates){
