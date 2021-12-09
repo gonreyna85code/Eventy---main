4cbb0b5e3267af2f27f9fde0a3446b5f2c58be93
gnunez-dev <gnunez.dev@gmail.com>
2021-12-09 17:34:42 -0500

    cloudinary config

--- client/src/hooks/useImage.js
+++ client/src/hooks/useImage.js
@@ -8,13 +8,7 @@ const useImage = () => {
         formData.append('file', file);
         formData.append('upload_preset', 'e2dcxjtm');
         formData.append("api_key", "228598698159782");
-
-        /*    formData.append("file", file);
-    
-    formData.append("eager", "w_400,h_300,c_pad|w_260,h_200,c_crop");
-    formData.append("public_id", "sample_image");
-    formData.append("timestamp", "1315060510");
-    formData.append("signature", "bfd09f95f331f558cbd1320e67aa8d488770583e"); */
+        formData.append("api_secret", "oHVjRT-xz5T6GP7Uq-82NaW1Ybc");
 
         let dataimg = await axios.post( 'https://api.cloudinary.com/v1_1/dbzyomisc/image/upload', formData );
         return dataimg.data.url;
