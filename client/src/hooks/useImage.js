ac5a20fb20fe77a630f67194f88e52b595a613d4
gnunez-dev <gnunez.dev@gmail.com>
2021-12-09 20:27:45 -0500

    .

--- client/src/hooks/useImage.js
+++ client/src/hooks/useImage.js
@@ -10,14 +10,9 @@ const useImage = () => {
         formData.append("api_key", "228598698159782");
         formData.append("api_secret", "oHVjRT-xz5T6GP7Uq-82NaW1Ybc");
 
-        //let dataimg = await axios.post( 'https://api.cloudinary.com/v1_1/dbzyomisc/image/upload', formData, {headers: { 'Content-Type': 'multipart/formdata', 'Access-Control-Allow-Origin': '*' }} );
-        
-        let url = 'https://api.cloudinary.com/v1_1/dbzyomisc/image/upload';
-        const image = await fetch(url, { method: "POST", body: formData });
-        const result = await image.json();
-        console.log('Response', result);
-        
-        return result.url;
+        let dataimg = await axios.post( 'https://api.cloudinary.com/v1_1/dbzyomisc/image/upload', formData, {
+            headers: { 'Content-Type': 'multipart/formdata', 'Access-Control-Allow-Origin': '*', "contentType": "Application/json", "crossDomain": true }} );
+        return dataimg.data.url;
     }
 
     return uploadImage;
