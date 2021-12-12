5ffd762ba0500e2550cbb27de06c4083ef49f434
gonzalo <gonreyna85@gmail.com>
2021-12-12 16:05:03 -0300

    fix

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -127,7 +127,7 @@ export function forgot(user) {
 export function reset(user) {
   return async function (dispatch) {
     try {
-      const id = user.id;
+      const id = user.id.id;
       console.log(id)
       const json = await axios({
         method: "POST",
