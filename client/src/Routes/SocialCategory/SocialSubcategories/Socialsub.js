4b3ca76a98693abda44fab1be9a30da2b9a199f1
gnunez-dev <gnunez.dev@gmail.com>
2021-12-14 09:15:39 -0500

    eliminación de suscripciones categorias

--- client/src/Routes/SocialCategory/SocialSubcategories/Socialsub.js
+++ client/src/Routes/SocialCategory/SocialSubcategories/Socialsub.js
@@ -1,46 +1,28 @@
 import React, {useEffect, useState} from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { findEventSub, subscription, unsubscription } from "../../../redux/actions";
+import { findEventSub, getUser } from "../../../redux/actions";
 import botonStyles from '../../../components/Boton/Boton.module.css';
-import Boton from '../../../components/Boton/Boton'
+//import Boton from '../../../components/Boton/Boton'
 import Card from "../../../components/CardEvent/CardEvent";
 
 
 export default function SubCategory(){
 
-    const {subcategory} = useParams();
-    console.log(subcategory);
-
     const dispatch = useDispatch();
+    const {subcategory} = useParams();
+    const eventos = useSelector((state) => state.Events);
+    const user= useSelector((state)=>state.User);
 
     useEffect(()=>{
         dispatch(findEventSub(subcategory)); 
     }, [dispatch, subcategory]);
 
-    const eventos = useSelector((state) => state.Events);
-    console.log(eventos)
-
-    var user= useSelector((state)=>state.User);
-    var [statusubs,setStatusubs]=useState('Suscribirse');
-
     useEffect(()=>{
-        if(user.subscriptions){
-            if(user.subscriptions.includes(subcategory)){
-                setStatusubs('Cancelar suscribción');
-            }
-        }
-    },[user,subcategory]);
-
-    function subscribe(e){
-        if(e.target.textContent==='Suscribirse'){
-            dispatch(subscription(user.username,e.target.id))
-            setStatusubs('Cancelar suscribción');
-        }else{
-            dispatch(unsubscription(user.username,e.target.id))
-            setStatusubs('Suscribirse');
-        }
-    }
+        dispatch(getUser())
+    }, [dispatch])
+    
+    
 
     return(
         <div>
@@ -63,9 +45,7 @@ export default function SubCategory(){
                     </div>
                 }
             </div>
-            <div className = 'boton_seguir'>
-                <Boton colorBtn='btn_naranja' children={statusubs} onClick={subscribe} id={subcategory} />
-            </div>
+            
             <div className = 'fondosub'>
                 <div className = 'siguenos'>
                     <h1>¡Síguenos en nuestro servidor de Discord y encuentra gente con tus mismos intereses!</h1>
--- client/src/Routes/SportCategory/SportCategory.js
+++ client/src/Routes/SportCategory/SportCategory.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {useDispatch, useSelector} from 'react-redux';
 import { Link } from "react-router-dom";
-import { findEventByCategory,subscription, unsubscription } from "../../redux/actions";
+import { findEventByCategory, getUser} from "../../redux/actions";
 import '../SocialCategory/SocialCategory.css'
 import botonStyles from "../../components/Boton/Boton.module.css"
 import Card from "../../components/CardEvent/CardEvent";
@@ -15,54 +15,35 @@ export default function SportCategory(){
     },[dispatch]);
 
     const sportsEvents = useSelector((state) => state.Events);
-    console.log(sportsEvents) 
-
-    var user=useSelector((state)=> state.User);
-    var [statusubs,setStatusubs]=useState({
-        sports:'Suscribirse',
-        Futbol:'Suscribirse',
-        Maraton:'Suscribirse',
-        Tenis:'Suscribirse',
-        Handball:'Suscribirse'
-    });
-
+    
     useEffect(()=>{
-        let subcategories=['Futbol','Maraton','Tenis','Handball'];
-        if(user.subscriptions){
-            if(user.subscriptions.includes('sports')){
-                setStatusubs({...statusubs,sports:'Cancelar suscribción'});
-            }
-            subcategories.forEach(e => {
-                if(user.subscriptions.includes(e)){
-                    setStatusubs({...statusubs,[e]:'Cancelar suscribción'});
-                }
-            });
-        }
-    },[user, statusubs]);
+        dispatch(getUser())
+    }, [dispatch])
 
-    function subscribe(e){
-        if(e.target.textContent==='Suscribirse'){
-            dispatch(subscription(user.username,e.target.id))
-            setStatusubs({...statusubs,[e.target.id]:'Cancelar suscribción'});
-        }else{
-            dispatch(unsubscription(user.username,e.target.id))
-            setStatusubs({...statusubs,[e.target.id]:'Suscribirse'});
-        }
-    }
 
     return(
         <div>
            <div className = 'encabezado'>
                 <h1 className = 'title'>Eventos Deportivos</h1>
-                <Boton colorBtn='btn_azul' children={statusubs.sports} onClick={subscribe} id='sports'/>
             </div>
             <div className ='cardsEvents'>
             {
-                sportsEvents && sportsEvents.map((el)=>{
+                sportsEvents && sportsEvents.length > 0 ? sportsEvents.map((el)=>{
                     return(
                         <Card key={el.name} img = {el.info.imagen} name = {el.name} location = {el.location.cityName} date = {el.date} id = {el.id} buttonColor='naranja'/>
                     )
                 })
+
+                : 
+
+                <div>
+                    <h3>Aún no hay eventos. Crea el tuyo!!</h3>
+                    <Link to = '/crear-evento'>
+                        <button className={`${botonStyles.btn} ${botonStyles.btn_azul}`}>
+                            Crear
+                        </button>
+                    </Link>
+                </div>
             }
             </div>
             <div className = 'cont-subcategories'>
@@ -70,7 +51,7 @@ export default function SportCategory(){
                 <div className = 'hobby'>
                     <h1 className = 'hobbytitle'>Fútbol</h1>
                     <section>
-                        <Boton colorBtn='btn_azul' children={statusubs.Futbol} onClick={subscribe} id='Futbol'/>
+                        
                         <Link to = {'/subcategory/Futbol'}>
                         <button className={`${botonStyles.btn} ${botonStyles.btn_azul}`}>
                             Acceder
@@ -81,7 +62,7 @@ export default function SportCategory(){
                 <div className = 'hobby'>
                     <h1 className = 'hobbytitle'>Maratón</h1>
                     <section>
-                        <Boton colorBtn='btn_azul' children={statusubs.Maraton} onClick={subscribe} id='Maraton'/>
+                        
                         <Link to = {'/subcategory/Maraton'}>
                         <button className={`${botonStyles.btn} ${botonStyles.btn_azul}`}>
                             Acceder
@@ -92,7 +73,7 @@ export default function SportCategory(){
                 <div className = 'hobby'>
                     <h1 className = 'hobbytitle'>Tenis</h1>
                     <section>
-                        <Boton colorBtn='btn_azul' children={statusubs.Tenis} onClick={subscribe} id='Tenis'/>
+                        
                         <Link to = {'/subcategory/Tenis'}>
                         <button className={`${botonStyles.btn} ${botonStyles.btn_azul}`}>
                             Acceder
@@ -103,7 +84,7 @@ export default function SportCategory(){
                 <div className = 'hobby'>
                     <h1 className = 'hobbytitle'>Handball</h1>
                     <section>
-                        <Boton colorBtn='btn_azul' children={statusubs.Handball} onClick={subscribe} id='Handball'/>
+                        
                         <Link to = {'/subcategory/Handball'}>
                         <button className={`${botonStyles.btn} ${botonStyles.btn_azul}`}>
                             Acceder
