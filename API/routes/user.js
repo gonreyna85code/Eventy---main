9fd018bc29171ec700d7274e82c7a3dd0ece565b
Alfredo-Calvelo <alfredocalvelo1@gmail.com>
2021-12-14 12:16:30 -0300

    Arreglo User Complete

--- API/routes/user.js
+++ API/routes/user.js
@@ -67,19 +67,41 @@ router.put("/user_update", isAuthenticated, (req, res, next) => {
     }
   });
 });
-
-
+router.get('/getAllUsers' ,async  (req,res )=>{
+  User.find((err,doc)=>{
+    res.send(doc)
+  })
+})
+router.get('/deleteuser', async(req,res)=>{
+  // User.findOne({_id: '61b7d4c0311c1e0023d32b35'},async(err,doc)=>{
+  //   res.send(doc)
+  // })
+  User.deleteOne({_id:'61b8ace916173c26ccc30247'}).then(()=>{res.send('Usuario eliminado')})
+})
 router.put('/userComplete',isAuthenticated, (req,res)=>{
-  // console.log(req.body.user.username);
   User.findOne({username:req.body.user.username}, async (err,doc)=>{
-    // const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(req.body.user.password, 10);
+    let user= req.body.user
     if(err) throw err;
     if(!doc) res.send('User Not Found')
     if(doc){ 
-      console.log(doc);
-      doc = req.body.user
-      console.log(doc);
-      await doc.save().then(()=>{
+      if(doc.email){
+        delete doc.email
+      }
+      doc.events= user.events
+      doc.follows = user.follows
+      doc.near= user.near
+      doc.password = hashedPassword
+      doc.payedEvents = user.payedEvents
+      doc.profile= user.profile
+      doc.promises = user.promises 
+      doc.selledEvents = user.selledEvents
+      doc.subscriptions = user.subscriptions
+      doc.username = user.username
+      doc.__v= user.__v
+      doc._id= user._id
+      doc.save()
+      .then(()=>{
         console.log('UserComplete');
         res.send(doc)
 
