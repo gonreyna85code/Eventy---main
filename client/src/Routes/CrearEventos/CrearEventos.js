3554e7ae5747447b313b4dbc98b712d646d9efd2
gonzalo <gonreyna85@gmail.com>
2021-12-15 19:07:18 -0300

    y otra prueba

--- client/src/Routes/CrearEventos/CrearEventos.js
+++ client/src/Routes/CrearEventos/CrearEventos.js
@@ -6,7 +6,7 @@ import Input from "../../components/Input/Input";
 import Select from "../../components/Select/Select";
 import Container from "../../components/Container/Container";
 import styles from "./CrearEventos.module.css";
-import { postEvent, getUser, changeEventCity } from "../../redux/actions";
+import { postEvent, getUser, changeEventCity, getEvent } from "../../redux/actions";
 import Map from "../../components/Maps/Map";
 import Warning from "../../components/Warning.js/Warning";
 import useImage from "../../hooks/useImage";
