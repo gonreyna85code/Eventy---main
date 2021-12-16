8a8d40896f9bd295795e7aaa00c3fc9f30a846c2
gonzalo <gonreyna85@gmail.com>
2021-12-16 00:54:23 -0300

    x

--- client/src/Routes/Profile/Information.js
+++ client/src/Routes/Profile/Information.js
@@ -8,7 +8,7 @@ export default function Information({user,handleClick}){
         <div className={styles.info}>
             <h3 className={styles.title}>Información</h3>
             <span> <b>User:</b> {user.username} </span>
-            <span> <b>Ciudad:</b> {user.profile?.city?.cityName} </span>
+            <span> <b>Ciudad:</b> {user.profile?.city?.city} </span>
             <span> <b>Edad:</b> {user.profile?.age} </span>
             <span> <b>Género:</b> {user.profile?.gender?user.profile?.gender:''}</span>
             <h3 onClick={handleClick} className={styles.title}>Usuarios seguidos:</h3>
